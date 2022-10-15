6bc9cf9ce671827294ed22f73ed9dc1d5589cc25
Diogo Sequeira <diogo.sequeira@ctw.bmwgroup.com>
2022-10-15 13:22:18 +0100

    add documentation

--- src/workshop/lib.rs
+++ src/workshop/lib.rs
@@ -1,8 +1,13 @@
 use scrypto::prelude::*;
 
+/// In Scrypto we have 2 different constants: Functions and Methods
+/// [new()] is a function as it does not require any Component State
+/// [buy()] is a method as it requires Component State (reference to self)
+/// It need to be mutable because we are gonna be mutating the component state (taking CTW tokens and XRD)
 blueprint{
     struct TokenSale {
         ctw_token_vault: Vault,
+        xrd_token_vault: Vault,
     }
 
     impl TokenSale {
@@ -14,9 +19,15 @@ blueprint{
         }
 
         Self {
+            /// Creates an empty vault and fills it with an initial bucket of resource.
             ctw_token_vault: Vault::with_bucket(bucket);
+            xrd_token_vault: Vault::new(RADIX_TOKEN);
         }
         .instantiate()
         .globalize()
+    }
+
+    pub fn buy(&mut self, funds: Bucket) => Bucket{
+        ß
     }
 }
