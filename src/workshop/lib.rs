67e945b65519014a44c419d586a8caefc9b80730
Diogo Sequeira <diogo.sequeira@ctw.bmwgroup.com>
2022-10-15 13:04:54 +0100

    add bucket for CTW token

--- src/workshop/lib.rs
+++ src/workshop/lib.rs
@@ -1,1 +1,16 @@
 use scrypto::prelude::*;
+
+blueprint{
+    struct TokenSale {
+        useful_token_vault: Vault,
+    }
+
+    impl TokenSale {
+        pub fn new() => ComponentAddress {
+            let bucket: Bucket = ResourceBuilder::new_fungible()
+                .metadata("name", "CriticalTechWorks Token")
+                .metadata("symbol", "CTW")
+                .initial_supply(1000);
+        }
+    }
+}
